9ca377efc383dd6fc33ff205f8f8f4ecbd313d30
Valdeci Gomes <valdeci@outlook.my>
2019-10-21 11:14:28 -0300

    Add readiness and liveness endpoints

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const health = require('@cloudnative/health-connect');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const healthcheck = new health.HealthChecker();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,27 +17,37 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const pingCheck = new health.PingCheck('example.com');
+
+//Liveness endpoint
+app.use('/live', health.LivenessEndpoint(healthcheck));
+// healthcheck.registerLivenessCheck(pingCheck);
+
+//Readiness endpoint
+app.use('/ready', health.ReadinessEndpoint(healthcheck));
+healthcheck.registerReadinessCheck(pingCheck);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
